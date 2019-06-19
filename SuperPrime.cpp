8bdcd116f3b2734467f7bece21b4830e06da6897
scathachcs <1428250238@qq.com>
2019-06-19 13:38:14 +0800

    Update SuperPrime.cpp
    超级素数cpp版

--- SuperPrime.cpp
+++ SuperPrime.cpp
@@ -1,29 +1,61 @@
-class Prime {
-public:
-  Prime(int num);
-  ~Prime();
+#include<iostream>
 
-  bool isPrime();
-private:
-  const int mNum;
+
+class prime {
+public:
+	int judgep(int a);
 };
-class SuperPrime : public Prime {
+
+class superprime {
 public:
-  SuperPrime(int num);
-  ~SuperPrime();
-
-  bool isPrime();
-
-  Prime sumBit();
-  Prime multiBit();
-  Prime sqaureSumBit();
+	superprime();
+	int judgesp(int i);
+	int show();
 private:
-  const int num;
-}
+	int num, max, sum;
+};
+
 int main()
 {
-    SuperPrime sp(113);
-    if(sp.isPrime()) {
-        ; // do something
-    }
+	superprime superprimex;
+	for (int i = 100; i < 1000; i++) {
+		superprimex.judgesp(i);
+	}
+	superprimex.show();
+	return 0;
+}
+
+int prime::judgep(int a) {
+	int i, flag = 1;
+	for (i = 2; i < a; i++) {
+		if (a % i == 0) {
+			flag = 0;
+			break;
+		}
+	}
+	if (a < 2)
+		flag = 0;
+	return flag;
+};
+int superprime::judgesp(int i) {
+	int a, b, c;
+	prime primex;
+	a = i / 100;
+	b = (i - a * 100) / 10;
+	c = i % 10;
+	if (primex.judgep(a + b + c) == 1 && primex.judgep(a * a + b * b + c * c) == 1 && primex.judgep(a * b * c) == 1) {
+		num++;
+		sum += i;
+		max = i;
+	};
+	return 0;
+}
+
+int superprime::show() {
+	std::cout<<max<<" "<<num<<" "<<sum<<std::endl;
+	return 0;
+}
+
+superprime::superprime() {
+	sum = 0, max = 0, num = 0;
 }
